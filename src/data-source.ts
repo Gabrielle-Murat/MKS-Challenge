5dfc48ca9623d3b3113fa236ab8ea6c533673055
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-22 21:10:20 -0300

    feat: Atualiza o data-source com as entidades e espaço para migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ import 'dotenv/config';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: 5432,
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
@@ -15,5 +15,5 @@ export const AppDataSource = new DataSource({
   logging: false,
   entities: [User, Movie],
   migrations: [],
-  subscribers: [],
+  //   subscribers: [],
 });
