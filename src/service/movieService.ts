aa54ac8136f264c8ba775cdedd13f7d30b7d68c6
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-24 16:58:26 -0300

    feat: Adiciona novos métodos ao service de Movie
    Para registrar novo filme, verificar sua existência no db e, buscar, atualizar e deletar um movie pelo Id da rota

--- src/service/movieService.ts
+++ src/service/movieService.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { AppDataSource } from '../database/data-source';
 import { Movie } from '../database/entity/Movie';
 import IMovie from '../interfaces/MovieInterface';
@@ -10,7 +10,97 @@ export default class MovieService {
       .select('movie')
       .from(Movie, 'movie')
       .getMany();
-    // return 'Movies are here';
     return movies;
+  }
+
+  async postMovie(movieInfo: IMovie): Promise<IMovie> {
+    const { title, genre, director, releaseYear, duration, rating, plot } =
+      movieInfo;
+    const doesMovieExist = await this.getMovie(movieInfo);
+
+    if (!doesMovieExist) {
+      const newMovie = await AppDataSource.createQueryBuilder()
+        .insert()
+        .into(Movie)
+        .values({
+          title,
+          genre,
+          director,
+          releaseYear,
+          duration,
+          rating,
+          plot,
+        })
+        .execute();
+
+      const newMovieId = newMovie.identifiers[0].id;
+      return {
+        id: newMovieId,
+        ...movieInfo,
+      };
+    }
+
+    throw new HttpException(
+      {
+        status: HttpStatus.BAD_REQUEST,
+        error: 'Movie already exists',
+      },
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+
+  async getMovie(movieInfo: IMovie): Promise<IMovie> {
+    const { title, director, releaseYear } = movieInfo;
+    const movie = await AppDataSource.createQueryBuilder()
+      .select('movie')
+      .from(Movie, 'movie')
+      .where('movie.title = :title', { title })
+      .andWhere('movie.director = :director', { director })
+      .andWhere('movie.releaseYear = :releaseYear', { releaseYear })
+      .getOne();
+
+    return movie;
+  }
+
+  async getMovieById(movieId: number): Promise<IMovie> {
+    const movie = await AppDataSource.createQueryBuilder()
+      .select('movie')
+      .from(Movie, 'movie')
+      .where('movie.id = :id', { id: movieId })
+      .getOne();
+
+    if (!movie) {
+      throw new HttpException('Movie not found', HttpStatus.NOT_FOUND);
+    }
+    return movie;
+  }
+
+  async updateMovie(movieId: number, movieInfo: any): Promise<any> {
+    const { title, genre, director, releaseYear, duration, rating, plot } =
+      movieInfo;
+
+    await AppDataSource.createQueryBuilder()
+      .update(Movie)
+      .set({
+        title,
+        genre,
+        director,
+        releaseYear,
+        duration,
+        rating,
+        plot,
+      })
+      .where('id = :id', { id: movieId })
+      .execute();
+
+    return 'Movie updated';
+  }
+
+  async deleteMovie(movieId: number): Promise<void> {
+    await AppDataSource.createQueryBuilder()
+      .delete()
+      .from(Movie)
+      .where('id = :id', { id: movieId })
+      .execute();
   }
 }
