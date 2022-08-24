2b3da3ceea9b336e7eec15e60b04f0535be51042
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-23 21:10:52 -0300

    feat: Atualiza o módulo com os services e controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import MovieController from './controller/movieController';
+import UserController from './controller/userController';
+import MovieService from './service/movieService';
+import UserService from './service/userService';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController, MovieController],
+  providers: [AppService, UserService, MovieService],
 })
 export class AppModule {}
