0b6af0cc75251bd2eed7e0dda1349635acafd7c5
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-25 21:41:11 -0300

    chore: Cria identity de genre para update futuro

---
+++ src/database/entity/Genre.ts
@@ -0,0 +1,14 @@
+// import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+// import { Movie } from './Movie';
+
+// @Entity('genres')
+// export class Genre {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column({ length: 20 })
+//   name: string;
+
+//   @OneToMany(() => Movie, (movie) => movie.genre)
+//   movies: Movie[];
+// }
