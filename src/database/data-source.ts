2848f39ccee60acc8427618a380934c361b32cc0
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-26 00:40:56 -0300

    feat: Modifica o data source
    Para utilizar a construção inicial de tabelas a partir de entities e a população as mesmas por migrations

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,11 +1,8 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-// import { User } from './entity/User';
-// import { Movie } from './entity/Movie';
+import { User } from './entity/User';
+import { Movie } from './entity/Movie';
 import 'dotenv/config';
-// import { CreateGenres1661450529772 } from './migrations/1661450529772-CreateGenres';
-import { CreateMovies1661458413905 } from './migrations/1661458413905-CreateMovies';
-import { CreateUsers1661458425098 } from './migrations/1661458425098-CreateUsers';
 // import { PopulateGenres1661457498137 } from './migrations/1661458527824-PopulateGenres';
 import { PopulateMovies1661458539009 } from './migrations/1661458539009-PopulateMovies';
 import { PopulateUsers1661458562767 } from './migrations/1661458562767-PopulateUsers';
@@ -19,11 +16,8 @@ export const AppDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE,
   synchronize: true,
   logging: false,
-  entities: [],
+  entities: [User, Movie],
   migrations: [
-    // CreateGenres1661450529772,
-    CreateMovies1661458413905,
-    CreateUsers1661458425098,
     // PopulateGenres1661457498137,
     PopulateMovies1661458539009,
     PopulateUsers1661458562767,
