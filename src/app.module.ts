bd07113e04b18748eb3eb28bec79745917881d7a
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-26 00:39:55 -0300

    feat: Adiciona autenticação de token para todas as rotas de movies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import LoginController from './controller/loginController';
 import MovieController from './controller/movieController';
 import UserController from './controller/userController';
+import { AuthMiddleware } from './middlewares/auth.middleware';
+import LoginService from './service/loginService';
 import MovieService from './service/movieService';
 import UserService from './service/userService';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController, MovieController],
-  providers: [AppService, UserService, MovieService],
+  controllers: [
+    AppController,
+    UserController,
+    MovieController,
+    LoginController,
+  ],
+  providers: [AppService, UserService, MovieService, LoginService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(MovieController);
+  }
+}
